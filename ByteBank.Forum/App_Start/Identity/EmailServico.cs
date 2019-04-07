dd98769b9777dde7715cc68937986a6c13ce03ac
yanomarcos <yanomarcos@gmail.com>
2019-04-06 22:49:46 -0300

    Servico de email

---
+++ ByteBank.Forum/App_Start/Identity/EmailServico.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ByteBank.Forum.App_Start.Identity
+{
+    public class EmailServico : IIdentityMessageService
+    {
+        private readonly string EMAIL_ORIGEM = ConfigurationManager.AppSettings["emailServico:email_remetente"];
+        private readonly string EMAIL_SENHA = ConfigurationManager.AppSettings["emailServico:email_senha"];
+
+        public async Task SendAsync(IdentityMessage message)
+        {
+            
+        }
+    }
+}
--- ByteBank.Forum/Controllers/ContaController.cs
+++ ByteBank.Forum/Controllers/ContaController.cs
@@ -45,7 +45,7 @@ namespace ByteBank.Forum.Controllers
                 novoUsuario.UserName = modelo.UserName;
                 novoUsuario.NomeCompleto = modelo.NomeCompleto;
 
-                var usuario = UserManager.FindByEmail(modelo.Email);
+                var usuario = await UserManager.FindByEmailAsync(modelo.Email);
                 var usuarioJaExiste = usuario != null;
 
                 if (usuarioJaExiste)

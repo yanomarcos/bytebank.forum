5e572b64850a7b4237f91d058b1403dbd553fd93
yanomarcos <yanomarcos@gmail.com>
2019-04-06 21:03:35 -0300

    configurando o owin

--- ByteBank.Forum/Controllers/ContaController.cs
+++ ByteBank.Forum/Controllers/ContaController.cs
@@ -1,10 +1,8 @@
 ﻿using ByteBank.Forum.Models;
 using ByteBank.Forum.ViewModels;
 using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
 using Microsoft.AspNet.Identity.EntityFramework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
@@ -13,6 +11,23 @@ namespace ByteBank.Forum.Controllers
 {
     public class ContaController : Controller
     {
+        private UserManager<UsuarioAplicacao> _userManager;
+        public UserManager<UsuarioAplicacao> UserManager
+        {
+            get
+            {
+                if (_userManager == null)
+                {
+                    var contextOwin = HttpContext.GetOwinContext();
+                    _userManager = contextOwin.GetUserManager<UserManager<UsuarioAplicacao>>();
+                }
+                return _userManager;
+            }
+            set
+            {
+                _userManager = value;
+            }
+        }
         public ActionResult Registrar()
         {
             return View();
@@ -33,7 +48,7 @@ namespace ByteBank.Forum.Controllers
                 novoUsuario.UserName = modelo.UserName;
                 novoUsuario.NomeCompleto = modelo.NomeCompleto;
 
-                await userManager.CreateAsync(novoUsuario, modelo.Senha);
+                await UserManager.CreateAsync(novoUsuario, modelo.Senha);
 
                 // Podemos incluir o usuário
                 return RedirectToAction("Index", "Home");

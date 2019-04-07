59fae66cf1fa80e677fc93c37fa6a28c99a5013f
yanomarcos <yanomarcos@gmail.com>
2019-04-07 12:07:39 -0300

    fim da parte 1

--- ByteBank.Forum/Startup.cs
+++ ByteBank.Forum/Startup.cs
@@ -50,6 +50,10 @@ namespace ByteBank.Forum
 
                     userManager.EmailService = new EmailServico();
 
+                    var dataProtectionProvider = opcoes.DataProtectionProvider;
+                    var dataProtectionProviderCreated = dataProtectionProvider.Create("ByteBank.Forum");
+
+                    userManager.UserTokenProvider = new DataProtectorTokenProvider<UsuarioAplicacao>(dataProtectionProviderCreated);
 
                     return userManager;
                 });
